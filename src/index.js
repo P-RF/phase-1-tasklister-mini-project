4b3186507f3e1461af522366bd95dc6110f01f5f
Priscila Rodriguez Fresco <priscila.rodriguezfresco@gmail.com>
2024-10-06 16:49:12 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    // Prevent default form submission
+    e.preventDefault();
+    handleToDo(e.target.elements['new-task-description'].value);
+    e.target.reset();
+  });
 });
+
+function handleToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+
+  btn.textContent = 'x';
+  btn.addEventListener('click', () => {
+    p.remove();
+});
+
+p.textContent = `${todo} `
+p.appendChild(btn);
+
+document.querySelector('#tasks').appendChild(p);
+}
